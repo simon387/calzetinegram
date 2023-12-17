ce8185b319da4b27dd7aa09662abba87c8729485
Simone Celia <simone.celia@simonecelia.it>
2023-12-17 14:59:01 +0100

    inti

---
+++ src/main/java/it/simonecelia/CalzetineGram/CalzetineGramApplication.java
@@ -0,0 +1,13 @@
+package it.simonecelia.CalzetineGram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalzetineGramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalzetineGramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/simonecelia/CalzetineGram/CalzetineGramApplicationTests.java
@@ -0,0 +1,13 @@
+package it.simonecelia.CalzetineGram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalzetineGramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
